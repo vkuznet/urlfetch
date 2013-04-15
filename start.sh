fb142fa9ec29fb88a538bd606eea118d9d4b6722
Valentin Kuznetsov <vkuznet@gmail.com>
2013-04-15 16:48:17 -0400

    Revisit start-up options

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd `dirname $0`
-# use this set of options when run on production node
-opts="-boot start_sasl -s reloader -s urlfetch -noshell"
 # use this set of options when run interactively (erlang will provide a shell)
 opts="-boot start_sasl -s reloader -s urlfetch"
-exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s urlfetch
+# use this set of options when run on production node
+opts="-boot start_sasl -s reloader -s urlfetch -noshell"
+echo "exec erl -pa $PWD/ebin $PWD/deps/*/ebin $opts"
+exec erl -pa $PWD/ebin $PWD/deps/*/ebin $opts
