29fbaaa90e9c022b16f6ee9fb56e773569541335
Valentin Kuznetsov <vkuznet@gmail.com>
2013-04-16 10:30:12 -0400

    Move processing code into separate module urlfetch_worker

--- src/urlfetch_resource.erl
+++ src/urlfetch_resource.erl
@@ -39,75 +39,9 @@ to_json(RD, Ctx) ->
 %%        -d "one=two&me=pope"
 process_post(RD, Ctx) ->
     Request = mochiweb_util:parse_qs(wrq:req_body(RD)),
-%    io:format("Request ~p~n", [Request]),
-    {ok, Data} = process_request(Request),
+    {ok, Data} = urlfetch_worker:process_request(Request),
     Body = mochijson:encode({struct, [{data,Data}]}),
     {true, wrq:append_to_response_body(Body, RD), Ctx}.
 
 json_body(QS) -> mochijson:encode({struct, QS}).
 
-%%
-%% Internal APIs
-%%
-
-process_request(L) ->
-    process_request(L, []).
-process_request([H|T], Results) ->
-%    io:format("process ~p~n", [H]),
-    case H of 
-        {"urls", Data} ->
-            R = process_urls(string:tokens(http_uri:decode(Data), "\n"));
-        _ ->
-%            io:format("No urls~n")
-            R = []
-    end,
-    process_request(T, Results++R);
-process_request([], Results) ->
-    {ok, parse_results(Results)}.
-
-parse_data(L) ->
-    NL = [binary_to_list(X) || X <- lists:flatten(L), is_binary(X), size(X) > 0],
-    string:join(NL, "").
-
-parse_results(L) ->
-    parse_results(L, []).
-parse_results([H|T], R) ->
-    case H of
-        {result, {Status, Data}} ->
-            case Status of
-                200 ->
-                    parse_results(T, R++parse_data(Data)++"\n")
-            end;
-        {error, not_found} ->
-            io:format("ERROR: No data~n")
-    end;
-parse_results([], R) -> R.
-
-process_urls(L) ->
-    process_urls(L, []).
-process_urls([Url|T], Results) ->
-    Id = urlfetch_uuid:new(),
-    io:format("process URL: ~p, uid: ~p~n", [Url, Id]),
-    Payload = "",
-    Headers = "",
-    Data = urlfetch_async:fetch({Id, get, Url, Payload, Headers}),
-    case Data of
-        ok ->
-            Output = get_result(Id);
-        error ->
-            Output = {error, not_found}
-    end,
-%    io:format("OUTPUT: ~p~n", [Output]),
-    process_urls(T, Results++[Output]);
-process_urls([], Results) -> Results.
-
-get_result(Id) ->
-    case urlfetch_async:get_result(Id) of
-        {result, Result} ->
-            {result, Result};
-        {error, retry} ->
-            timer:sleep(50),
-            get_result(Id);
-        {error, not_found} ->
-            {error, not_found}
-    end.
