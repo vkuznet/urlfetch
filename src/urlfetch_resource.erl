0f7206be1bd0274d379c11b5123d536b4b626fe9
Valentin Kuznetsov <vkuznet@gmail.com>
2013-04-22 10:58:54 -0400

    Re-design process_post request to return data string instead of JSON; add examples to process text requests, etc.

--- src/urlfetch_resource.erl
+++ src/urlfetch_resource.erl
@@ -14,6 +14,7 @@
          allowed_methods/2,
          content_types_provided/2,
          to_json/2,
+         to_text/2,
          process_post/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
@@ -26,22 +27,37 @@ init([]) ->
 allowed_methods(RD, Ctx) ->
     {['GET', 'HEAD', 'POST'], RD, Ctx}.
 
-content_types_provided(RD, Ctx) ->
-    {[{"application/json", to_json}], RD, Ctx}.
+%content_types_provided(RD, Ctx) ->
+%    {[{"application/json", to_json}], RD, Ctx}.
 
-%% hit this with
-%%   curl "http://localhost:8000/formjson?one=two&me=pope"
+content_types_provided(ReqData, Context) ->
+    {[{"application/json",to_json},{"text/plain",to_text}], ReqData, Context}.
+
+%% Handle GET request
+to_text(ReqData, Context) ->
+    Path = wrq:disp_path(ReqData),
+    Body = io_lib:format("Hello ~s from webmachine.~n", [Path]),
+    {Body, ReqData, Context}.
+
+%% Handle GET request using application/json header, e.g.
+%% curl "http://localhost:8000/formjson?one=two&me=pope"
 to_json(RD, Ctx) ->
     {json_body(wrq:req_qs(RD)), RD, Ctx}.
 
-%% hit this with
-%%   curl -X POST http://localhost:8000/formjson \
-%%        -d "one=two&me=pope"
+%% Handle POST request, e.g.
+%% curl -X POST http://localhost:8000/formjson -d "one=two&me=pope"
 process_post(RD, Ctx) ->
     Request = mochiweb_util:parse_qs(wrq:req_body(RD)),
     {ok, Data} = urlfetch_worker:process_request(Request),
-    Body = mochijson:encode({struct, [{data,Data}]}),
-    {true, wrq:append_to_response_body(Body, RD), Ctx}.
+    {true, wrq:append_to_response_body(Data, RD), Ctx}.
+
+%% Handle POST request and return data in JSON format, {"data":Data}
+%% curl -X POST http://localhost:8000/formjson -d "one=two&me=pope"
+%process_post(RD, Ctx) ->
+%    Request = mochiweb_util:parse_qs(wrq:req_body(RD)),
+%    {ok, Data} = urlfetch_worker:process_request(Request),
+%    Body = mochijson:encode({struct, [{data,Data}]}),
+%    {true, wrq:append_to_response_body(Body, RD), Ctx}.
 
 json_body(QS) -> mochijson:encode({struct, QS}).
 
